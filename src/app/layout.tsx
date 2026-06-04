385d6a4a792ee4dfa278ba07fdbee4758b6c0180
vx4ssty7nn-web <vx4ssty7nn@privaterelay.appleid.com>
2026-06-04 18:16:36 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { AppSessionProvider } from "@/components/session-provider";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body>
-        <AppSessionProvider>{children}</AppSessionProvider>
+     {children}
       </body>
     </html>
   );
