8fbbdc032e42e43321496c9b033efed034e2807f
vx4ssty7nn-web <vx4ssty7nn@privaterelay.appleid.com>
2026-06-04 20:48:00 +0900

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1>Webサイトが正常に公開されました！</h1>
+      <p>Next.js × Azure Static Web Appsのデプロイに成功しています。</p>
+    </div>
+  );
+}
